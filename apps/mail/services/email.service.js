f2a05eaff9c4861b644e56da9a8b542f35fa6ae0
Gal Lankri <gal.lankri2@gmail.com>
2022-11-09 13:18:43 +0200

    upload email data to email.json and put on email.service

--- apps/mail/services/email.service.js
+++ apps/mail/services/email.service.js
@@ -1,68 +1,65 @@
 import { utilService } from '/services/util.service.js'
 import { storageService } from '/services/async-storage.service.js'
-
-import gEmails from '../../data/emails.json' assert {type: 'json'}
+import emailsList from '../../../data/email.json' assert { type: 'json' }
 
 const EMAILS_KEY = 'emailsDB'
 
 _createEmails()
 
 export const emailService = {
-    query,
-    get,
-    remove,
-    save,
-    getNextEmailId,
-    getPrevEmailId,
+  query,
+  get,
+  remove,
+  save,
+  getNextEmailId,
+  getPrevEmailId,
 }
 
 function query() {
-    return storageService.query(EMAILS_KEY)
+  return storageService.query(EMAILS_KEY)
 }
 
 function get(emailId) {
-    console.log(`emailId:`, emailId)
-    return storageService.get(EMAILS_KEY, emailId)
+  console.log(`emailId:`, emailId)
+  return storageService.get(EMAILS_KEY, emailId)
 }
 
 function remove(emailId) {
-    return storageService.remove(EMAILS_KEY, emailId)
+  return storageService.remove(EMAILS_KEY, emailId)
 }
 
 function save(email) {
-    console.log(`email.id:`, email.id)
-    if (email.id) {
-        return storageService.put(EMAILS_KEY, email)
-    } else {
-        return storageService.post(EMAILS_KEY, email)
-    }
+  console.log(`email.id:`, email.id)
+  if (email.id) {
+    return storageService.put(EMAILS_KEY, email)
+  } else {
+    return storageService.post(EMAILS_KEY, email)
+  }
 }
 
 function _createEmails() {
-    let emails = utilService.loadFromStorage(EMAILS_KEY)
+  let emails = utilService.loadFromStorage(EMAILS_KEY)
+  if (!emails || !emails.length) {
+    emails = emailsList
+    utilService.saveToStorage(EMAILS_KEY, emails)
     console.log(emails)
-    if (!emails || !emails.length) {
-        emails = gEmails
-        utilService.saveToStorage(EMAILS_KEY, gEmails)
-    }
+  }
 }
 
 function getNextEmailId(emailId) {
-    return storageService.query(EMAILS_KEY)
-        .then(emails => {
-            var idx = emails.findIndex(email => email.id === emailId)
-            if (idx === emails.length - 1) idx = -1
-            return emails[idx + 1].id
-        })
+  return storageService.query(EMAILS_KEY).then((emails) => {
+    var idx = emails.findIndex((email) => email.id === emailId)
+    if (idx === emails.length - 1) idx = -1
+    return emails[idx + 1].id
+  })
 }
 
 function getPrevEmailId(emailId) {
-    return storageService.query(EMAILS_KEY)
-        .then(emails => {
-            var idx = emails.findIndex(email => email.id === emailId)
-            if (idx === 0) idx = emails.length
-            return emails[idx - 1].id
-        })
+  return storageService.query(EMAILS_KEY).then((emails) => {
+    var idx = emails.findIndex((email) => email.id === emailId)
+    if (idx === 0) idx = emails.length
+    return emails[idx - 1].id
+  })
 }
 
 // function _createCars() {
