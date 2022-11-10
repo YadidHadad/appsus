5e9f763831eeb53317909bf48c6648086ec693c6
Yadid Hadad <yadidhadad@gmail.com>
2022-11-10 11:08:48 +0200

    update

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -3,22 +3,22 @@ export default {
         <section class="home-page">
             <h1>Welcome to Appsus</h1>
             <div class="navbar">
-            <nav class="flex row gap align-center justify-center">
-                <router-link to="/">
-                    <img src="../assets/img/header/home.svg" alt="" />
-                </router-link>
-                <router-link to="/books">
-                    <img src="../assets/img/header/scholar.png" alt="" />
-                </router-link>
-                <router-link to="/email">
-                    <img src="../assets/img/header/gmail.png" alt="" />
-                </router-link>
-                <router-link to="/note">
-                    <img src="../assets/img/header/keep.png" alt="" />
-                </router-link>
-            </nav>
-        </div>
-        </div>
+                <nav class="flex row gap align-center justify-center">
+                    <router-link to="/">
+                        <img src="/assets/img/header/home.svg" alt="" />
+                    </router-link>
+                    <router-link to="/books">
+                        <img src="/assets/img/header/scholar.png" alt="" />
+                    </router-link>
+                    <router-link to="/email">
+                        <img src="/assets/img/header/gmail.png" alt="" />
+                    </router-link>
+                    <router-link to="/note">
+                        <img src="/assets/img/header/keep.png" alt="" />
+                    </router-link>
+                </nav>
+            </div>
+  
         </section>
     `,
 }
